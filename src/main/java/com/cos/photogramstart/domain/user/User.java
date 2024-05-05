f566046b382b598faee77c2459e9ba452f035264
jisupark1992 <jisupark1992@gmail.com>
2024-05-05 14:01:25 +0900

    [feat] 회원가입완료 및 패스워드 해시 구조

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.domain.user;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,11 +10,13 @@ import javax.persistence.Id;
 import javax.persistence.PrePersist;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 // JPA - Java Persistence API (자바로 데이터를 영구적으로 저장(DB)할 수 있는 API 제공)
 
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -23,6 +26,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //번호 증가 전략이 데이터베이스를 따라간다.
 	private int id;
 	
+	@Column(unique = true)
 	private String username;
 	private String password;
 	private String name;
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor // final 필드를 DI 할 때 사용
 @Controller //1. IoC  2. 파일을 리턴하는 컨트롤러
 public class AuthController {
+	private final AuthService authService;
+	
+//	public AuthController(AuthService authService) {
+//		this.authService = authService;
+//	}
 	
 	@GetMapping("/auth/signin")
 	public String signinForm() {
@@ -22,6 +32,8 @@ public class AuthController {
 	// 회원가입버튼
 	@PostMapping("/auth/signup")
 	public String signup(SignupDto signupDto) {
+		User user = signupDto.toEntity();
+		User userEntity = authService.회원가입(user);
 		return "auth/signin";
 	}
 }
