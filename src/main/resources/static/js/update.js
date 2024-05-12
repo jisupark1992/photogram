17e1ab3c4aa8891306136649cf0af805c108cf85
jisupark1992 <jisupark1992@gmail.com>
2024-05-13 00:58:37 +0900

    [feat] 세션정보 확인 및 회원정보 수정(시큐리티, ajax)

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,4 +1,16 @@
 // (1) 회원정보 수정
-function update() {
+function update(userId) {
+    let data = $("#profileUpdate").serialize();
 
+    $.ajax({
+        type:"put",
+        url:`apu/user/${userId}`,
+        data: data,
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        dataType:"json"
+    }).done(res=>{
+        console.log("update 성공");
+    }).fail(error=>{
+        console.log("update 실패");
+    });
 }
