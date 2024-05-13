1e0df0d9d6d608183bf74d8dec0b16086e2399fb
DESKTOP-UCV7THP\박지수 <pjs-cjswo@daonlink.com>
2024-05-13 12:42:23 +0900

    [feat] 구독 모델 및 API 제적 (구독/ 구독취소)

---
+++ src/main/java/com/cos/photogramstart/domain/subscribe/Subscribe.java
@@ -0,0 +1,46 @@
+package com.cos.photogramstart.domain.subscribe;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// JPA - Java Persistence API (자바로 데이터를 영구적으로 저장(DB)할 수 있는 API 제공)
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity // 디비에 테이블을 생성
+@Table(
+	uniqueConstraints = {
+			@UniqueConstraint(
+					name="subscribe_uk",
+					columnNames = {"fromUserId", "toUserId"}
+			)
+	}
+)
+public class Subscribe {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //번호 증가 전략이 데이터베이스를 따라간다.
+	private int id;
+
+	@JoinColumn(name = "fromUser")
+	@ManyToOne
+	private User fromUser;
+
+	@JoinColumn(name = "toUser")
+	@ManyToOne
+	private User toUser;
+
+	private LocalDateTime createDate;
+
+	@PrePersist
+	public void createDate(){
+		this.createDate = LocalDateTime.now();
+	}
+}
---
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -0,0 +1,28 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.domain.subscribe.Subscribe;
+import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class SubscribeService {
+
+    private final SubscribeRepository subscribeRepository;
+
+    @Transactional
+    public void 구독하기(int fromUserId, int toUserId) {
+        subscribeRepository.mSubscribe(fromUserId, toUserId);
+    }
+
+    @Transactional
+    public void 구독취소하기(int fromUserId, int toUserId) {
+        subscribeRepository.mUnSubscribe(fromUserId, toUserId);
+    }
+}
