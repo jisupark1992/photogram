887043aed9b6b2c3d74268cfe7957e036a9cf505
DESKTOP-UCV7THP\박지수 <pjs-cjswo@daonlink.com>
2024-05-13 11:59:32 +0900

    [feat] Optional 처리로 수정 마무ë리 및 구독 연관관계 이해

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -13,6 +13,10 @@ function update(userId, event) {
         console.log("성공", res);
         location.href = "/user/" + userId;
     }).fail(error=>{
-        alert(JSON.stringify(error.responseJSON.data));
+        if(error.data == null){
+            alert(error.responseJSON.message);
+        }else{
+            alert(JSON.stringify(error.responseJSON.data));
+        }
     });
 }
